329da65778222685950f8a3f5e3dc6ccd81993a5
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-02-06 11:52:55 +0200

    uploadjson

--- ts-test/dist/index.js
+++ ts-test/dist/index.js
@@ -1,3 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const permissions_json_1 = require("./permissions.json");
+function getUserPermissions(user) {
+    if (user.toLowerCase() === "shirley") {
+        return permissions_json_1.permissions.find((item) => item.id === 5);
+    }
+    else {
+        return permissions_json_1.permissions[0];
+    }
+}
+const result = getUserPermissions("shirley");
+console.log(result);
 const netApp = "This is Netapp course";
 console.log(netApp);
 console.log("this is a new message");
@@ -5,9 +18,14 @@ const obj = {
     company: "netApp",
     location: "Ehad Haam 9",
 };
-console.log(obj);
-console.log(obj);
-console.log(obj);
-console.log(obj);
-console.log(obj);
-console.log(obj);
+const buildingNumber = 9;
+const users = ["Shirley", "dudu", "liron"];
+users.push("Vova");
+const usersArrayOption2 = ["Shirley", "dudu", "liron"];
+const isHost = true;
+let MAX_RECORDS = 50000;
+const vLocation = "Dubai";
+const fromDate = new Date();
+const toDate = new Date().toISOString();
+console.log(vLocation, `toDate: ${fromDate}`, `toDate: ${toDate}`);
+console.log(users);
--- ts-test/src/index.ts
+++ ts-test/src/index.ts
@@ -1,3 +1,19 @@
+import fileJson from "./settings.json";
+import { permissions } from "./permissions.json";
+type Permission = (typeof permissions)[0];
+
+function getUserPermissions(user: string): Permission {
+  if (user.toLowerCase() === "shirley") {
+    return permissions.find((item) => item.id === 5);
+  } else {
+    return permissions[0];
+  }
+}
+
+const result = getUserPermissions("shirley");
+console.log(result);
+type SettingsType = typeof fileJson;
+
 const netApp: string = "This is Netapp course";
 console.log(netApp);
 console.log("this is a new message");
@@ -6,9 +22,18 @@ const obj: { company: string; location: string } = {
   company: "netApp",
   location: "Ehad Haam 9",
 };
-console.log(obj);
-console.log(obj);
-console.log(obj);
-console.log(obj);
-console.log(obj);
-console.log(obj);
+
+const buildingNumber: number = 9;
+const users: Array<string> = ["Shirley", "dudu", "liron"];
+users.push("Vova");
+
+const usersArrayOption2: string[] = ["Shirley", "dudu", "liron"];
+const isHost: boolean = true;
+
+let MAX_RECORDS: number = 50000;
+
+const vLocation: string = "Dubai";
+const fromDate: Date = new Date();
+const toDate: string = new Date().toISOString();
+console.log(vLocation, `toDate: ${fromDate}`, `toDate: ${toDate}`);
+console.log(users);
