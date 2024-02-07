e895fd7f85b4079f8a1c934a68a1b1a4b03ed715
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-02-07 10:18:50 +0200

    a

--- ts-test/dist/signatureCall.js
+++ ts-test/dist/signatureCall.js
@@ -12,4 +12,18 @@ RunningSomeCode(extractEmail);
 extractEmail.defaultDomain = "@hotmail.com";
 RunningSomeCode(extractEmail);
 console.log(users.map(extractEmail));
+const numbers = [1, 4, 3, 2, 5, 6, 5, 4, 5, 2, 2, 3, 8, 9];
+sortFn.sortKey = "asc";
+function sortFn(a, b) {
+    if (sortFn.sortKey === "asc")
+        return a - b;
+    else
+        return b - a;
+}
+runSortMain(sortFn);
+sortFn.sortKey = "desc";
+runSortMain(sortFn);
+function runSortMain(callBackSortFn) {
+    console.log(numbers.sort(callBackSortFn));
+}
 exports.default = 1;
--- ts-test/src/signatureCall.ts
+++ ts-test/src/signatureCall.ts
@@ -21,4 +21,25 @@ RunningSomeCode(extractEmail);
 extractEmail.defaultDomain = "@hotmail.com";
 RunningSomeCode(extractEmail);
 console.log(users.map(extractEmail));
+
+// - Create `SortFunction` interface with sortBy key - `asc` | `desc` and function that can
+
+interface SortFunction {
+  (a: number, b: number): number;
+  sortKey: string;
+}
+const numbers = [1, 4, 3, 2, 5, 6, 5, 4, 5, 2, 2, 3, 8, 9];
+sortFn.sortKey = "asc";
+function sortFn(a: number, b: number) {
+  if (sortFn.sortKey === "asc") return a - b;
+  else return b - a;
+}
+runSortMain(sortFn);
+sortFn.sortKey = "desc";
+runSortMain(sortFn);
+
+function runSortMain(callBackSortFn: SortFunction) {
+  console.log(numbers.sort(callBackSortFn));
+}
+
 export default 1;
