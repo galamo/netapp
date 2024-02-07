7b615610969ec7042196c2561aaf3e3baa4fea44
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-02-07 11:16:43 +0200

    a

--- ts-test/dist/omitAndPick.js
+++ ts-test/dist/omitAndPick.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.filterUsers = exports.logPerson = exports.isUser = exports.isAdmin = exports.persons = void 0;
 const user = {
     password: "1234",
     firstName: "gal",
@@ -5,3 +8,75 @@ const user = {
     userName: "galamo88@gmail.com",
     roles: ["Admin", "User"],
 };
+const pu = {
+    name: "",
+    age: 1,
+    role: "",
+    lastVisit: "",
+    type: "PowerUser",
+};
+exports.persons = [
+    {
+        type: "user",
+        name: "Max Mustermann",
+        age: 25,
+        lastVisit: "Before 10 hours",
+    },
+    {
+        type: "admin",
+        name: "Jane Doe",
+        age: 32,
+        role: "Administrator",
+    },
+    {
+        type: "user",
+        name: "Kate Müller",
+        age: 23,
+        lastVisit: "Before 10 hours",
+    },
+    {
+        type: "admin",
+        name: "Bruce Willis",
+        age: 64,
+        role: "World saver",
+    },
+    {
+        type: "user",
+        name: "Wilson",
+        age: 23,
+        lastVisit: "Before 10 hours",
+    },
+    {
+        type: "admin",
+        name: "Agent Smith",
+        age: 23,
+        role: "Administrator",
+    },
+];
+const isAdmin = (person) => person.type === "admin";
+exports.isAdmin = isAdmin;
+const isUser = (person) => person.type === "user";
+exports.isUser = isUser;
+function logPerson(person) {
+    let additionalInformation = "";
+    if ((0, exports.isAdmin)(person)) {
+        additionalInformation = person.role;
+    }
+    if ((0, exports.isUser)(person)) {
+        additionalInformation = person.lastVisit;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+exports.logPerson = logPerson;
+function filterUsers(persons, criteria) {
+    return persons.filter(exports.isUser).filter((user) => {
+        const criteriaKeys = Object.keys(criteria);
+        return criteriaKeys.every((fieldName) => {
+            return user[fieldName] === criteria[fieldName];
+        });
+    });
+}
+exports.filterUsers = filterUsers;
+filterUsers(exports.persons, {
+    name: "Wilson",
+}).forEach(logPerson);
--- ts-test/src/omitAndPick.ts
+++ ts-test/src/omitAndPick.ts
@@ -17,3 +17,115 @@ const user: SuperUser = {
 type Subscriber = Omit<SuperUser, "roles" | "firstName" | "lastName">;
 
 type AutneticatedSubscriber = Pick<SuperUser, "roles" | "userName">;
+
+interface User {
+  type: "user";
+  name: string;
+  age: number;
+  lastVisit: string;
+}
+
+interface Admin {
+  type: "admin";
+  name: string;
+  age: number;
+  role: string;
+}
+
+type PowerUser = Omit<User, "type"> &
+  Omit<Admin, "type"> & { type: "PowerUser" };
+const pu: PowerUser = {
+  name: "",
+  age: 1,
+  role: "",
+  lastVisit: "",
+  type: "PowerUser",
+};
+
+interface User {
+  type: "user";
+  name: string;
+  age: number;
+  lastVisit: string;
+}
+
+interface Admin {
+  type: "admin";
+  name: string;
+  age: number;
+  role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+  {
+    type: "user",
+    name: "Max Mustermann",
+    age: 25,
+    lastVisit: "Before 10 hours",
+  },
+  {
+    type: "admin",
+    name: "Jane Doe",
+    age: 32,
+    role: "Administrator",
+  },
+  {
+    type: "user",
+    name: "Kate Müller",
+    age: 23,
+    lastVisit: "Before 10 hours",
+  },
+  {
+    type: "admin",
+    name: "Bruce Willis",
+    age: 64,
+    role: "World saver",
+  },
+  {
+    type: "user",
+    name: "Wilson",
+    age: 23,
+    lastVisit: "Before 10 hours",
+  },
+  {
+    type: "admin",
+    name: "Agent Smith",
+    age: 23,
+    role: "Administrator",
+  },
+];
+
+export const isAdmin = (person: Person): person is Admin =>
+  person.type === "admin";
+export const isUser = (person: Person): person is User =>
+  person.type === "user";
+
+export function logPerson(person: Person) {
+  let additionalInformation = "";
+  if (isAdmin(person)) {
+    additionalInformation = person.role;
+  }
+  if (isUser(person)) {
+    additionalInformation = person.lastVisit;
+  }
+  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+type SearchCriteria = Partial<Pick<User, "name" | "age">>;
+
+export function filterUsers(
+  persons: Person[],
+  criteria: SearchCriteria
+): User[] {
+  return persons.filter(isUser).filter((user) => {
+    const criteriaKeys = Object.keys(criteria) as (keyof SearchCriteria)[];
+    return criteriaKeys.every((fieldName) => {
+      return user[fieldName] === criteria[fieldName];
+    });
+  });
+}
+
+filterUsers(persons, {
+  name: "Wilson",
+}).forEach(logPerson);
